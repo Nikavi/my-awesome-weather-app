c7904529d139848e0c1e669a06adb6e43bf70531
Nikavi <104579294+Nikavi@users.noreply.github.com>
2022-09-19 12:17:47 +0200

    Added relevant icon from API

--- index.js
+++ index.js
@@ -93,8 +93,14 @@ function showWeather(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+  let iconElement = document.querySelector("#icon");
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    'http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png'
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 function searchCity(event) {
   event.preventDefault();
